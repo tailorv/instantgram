7b559218fcf494b88b14f016fcf12863337db381
tailorv <maxwellmuthomijr@gmail.com>
2021-10-18 10:10:29 +0300

    create token generator

--- instagram/token_generator.py
+++ instagram/token_generator.py
@@ -7,4 +7,4 @@ class TokenGenerator(PasswordResetTokenGenerator):
         return (text_type(user.is_active)+text_type(user.pk)+text_type(timestamp))
 
 
-account_activation_token = TokenGenerator()
+account_activation_token = TokenGenerator(
