4c42a6de0c91ec426d31194d1a1521ec407378d2
tailorv <maxwellmuthomijr@gmail.com>
2021-10-18 10:38:16 +0300

    create email.py file

--- instagram/email.py
+++ instagram/email.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
     subject = 'Welcome to the Instagram Look-alike'
-    sender = 'maxwellmuthomijr@gmail.com'
+    sender = 'maxwellmuthomjr@gmail.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/newsemail.txt',{"name": name})
