2d0fe0242dadb10196ff5bce34129f83e364a2a0
tailorv <maxwellmuthomijr@gmail.com>
2021-10-19 13:15:56 +0300

    deployment

--- socials/settings.py
+++ socials/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
 from pathlib import Path
 from decouple import config
 import django_heroku
@@ -24,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*6p+#tkrn0*4)jov@zd6)ulv+!z(u8gl%1&^#m-=)@6(rpq(3v'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
